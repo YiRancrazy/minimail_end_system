dc6284fb483e3676178874067f5f5206335e72a9
YiRanCrazy <yirancrazy@gmail.com>
2026-07-30 19:30:37 +0800

    feat(pay): 新增支付宝 SDK 依赖和配置类

---
+++ minimall-pay-service/src/main/java/com/yirancrazy/minimall/pay/config/AlipayConfig.java
@@ -0,0 +1,37 @@
+package com.yirancrazy.minimall.pay.config;
+
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "minimall.alipay")
+public class AlipayConfig {
+
+    private String appId;
+    private String privateKey;
+    private String alipayPublicKey;
+    private String gatewayUrl;
+    private String notifyUrl;
+    private String returnUrl;
+    private String format = "json";
+    private String charset = "UTF-8";
+    private String signType = "RSA2";
+
+    public void setAppId(String appId) { this.appId = appId; }
+    public void setPrivateKey(String privateKey) { this.privateKey = privateKey; }
+    public void setAlipayPublicKey(String alipayPublicKey) { this.alipayPublicKey = alipayPublicKey; }
+    public void setGatewayUrl(String gatewayUrl) { this.gatewayUrl = gatewayUrl; }
+    public void setNotifyUrl(String notifyUrl) { this.notifyUrl = notifyUrl; }
+    public void setReturnUrl(String returnUrl) { this.returnUrl = returnUrl; }
+
+    @Bean
+    public AlipayClient alipayClient() {
+        return new DefaultAlipayClient(
+            gatewayUrl, appId, privateKey, format, charset, alipayPublicKey, signType);
+    }
+}

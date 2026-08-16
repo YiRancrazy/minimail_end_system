f17ee8856e8d52ee3226969fb07785234dd5f106
YiRanCrazy <yirancrazy@gmail.com>
2026-08-16 20:06:57 +0800

    fix(api): 补齐IdFeignClient的@FeignClient注解

--- minimall-api/src/main/java/com/yirancrazy/minimall/api/feign/IdFeignClient.java
+++ minimall-api/src/main/java/com/yirancrazy/minimall/api/feign/IdFeignClient.java
@@ -1,15 +1,18 @@
 package com.yirancrazy.minimall.api.feign;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.yirancrazy.minimall.api.fallback.IdFeignFallbackFactory;
 import com.yirancrazy.minimall.common.result.Result;
 
 /**
  * @Author: yirancrazy@gmail.com
  * @Description: Id Feign 客户端，调用Id服务接口
- * @Version: 1.1
+ * @Version: 1.2
  * @DateTime: 2026/08/02
  */
+@FeignClient(name = "minimall-id-service", fallbackFactory = IdFeignFallbackFactory.class)
 public interface IdFeignClient {
     /**
      * 根据业务标签获取下一个全局唯一 ID。

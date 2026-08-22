5d2a5a69bb0c3b7fca7530a87d8f7453f712195d
YiRanCrazy <yirancrazy@gmail.com>
2026-08-22 08:29:27 +0800

    fix(goods): 启用FeignClients扫描注册MerchantFeignClient

--- minimall-goods-service/src/main/java/com/yirancrazy/minimall/goods/GoodsApplication.java
+++ minimall-goods-service/src/main/java/com/yirancrazy/minimall/goods/GoodsApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Author: yirancrazy@gmail.com
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @MapperScan("com.yirancrazy.minimall.goods.mapper")
+@EnableFeignClients(basePackages = "com.yirancrazy.minimall.api.feign")
 public class GoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class, args);
